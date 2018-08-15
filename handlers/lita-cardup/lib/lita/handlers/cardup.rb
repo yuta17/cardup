c23f142ee990f4b8bc5bb39611542d11cdaf28a6
Hassan <usgitan@gmail.com>
2018-08-15 15:19:12 +0900

    create and response card

--- handlers/lita-cardup/lib/lita/handlers/cardup.rb
+++ handlers/lita-cardup/lib/lita/handlers/cardup.rb
@@ -2,10 +2,42 @@ module Lita
   module Handlers
     class Cardup < Handler
       # insert handler code here
-      route /こんにちは/, :hello
+      route /(.+)/, :reply_card
 
-      def hello(response)
-        response.reply('こんにちは！今日も頑張ろう！')
+      def reply_card(response)
+        target_word = response.matches[0][0]
+        card = create_card(target_word)
+        response.reply(card)
+      end
+
+      def create_card(word)
+        <<-"EOS"
+          ```
+          
+          `             `  `` ...-J??"^`  Z[      `.?           .
+          `     `` ` ...-+?7"^`          `X`    `.(!            .
+          `   ` .Y7!!`                    (;` ` .!              .
+          `   ` r`                         b``.!   `            .
+          `     d.                       ` j    `        `      .
+          `    `(@`                       `.;                 ` .
+          `      h`                       ` b        `     `    .
+          `     `(-     #{word}              `d.                  .
+          `  `    [`    が全く分からない      `.]`    `            .
+          `       u.    時に挙げる札           4          `       .
+          `       ,|`                        ,.            `    .
+          `        S                         .r`   `            .
+          `       `(_                        `W `               .
+          .       ` [`                        ,|        `       .
+          _  `      q                          b`    `     `    .
+          ?<-.``    ,[                         (                .
+          .  `.?+.. `S                       ` ,|  `            .
+          _!!-...``_!(_                       ` b``      `    ` .
+          _     `   `.L                       `J-   `  ....-~??7
+          `           O.                      `.[  ` ~`        .
+          `          `,]                        z              .
+          `          ` 1                       `(-             
+          ```
+        EOS
       end
 
       Lita.register_handler(self)
